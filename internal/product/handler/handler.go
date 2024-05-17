362cb2624719d0dad2f0b8498bb2baf39ccf5204
neiltonrodriguez <neiltonrodriguez@gmail.com>
2024-05-17 19:19:59 -0300

    implementação dos logs

--- internal/product/handler/handler.go
+++ internal/product/handler/handler.go
@@ -1,19 +1,23 @@
 package handler
 
 import (
+	"eulabs/common"
 	"eulabs/domain"
 	ProductModel "eulabs/models/product"
 	"net/http"
 
 	"github.com/labstack/echo"
+	"github.com/rs/zerolog/log"
 )
 
 func GetAll(c echo.Context) error {
 	products, err := ProductModel.GetAll(c.Request().Context())
 	if err != nil {
+		log.Error().Msg("Error in GetAll of product: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
+	common.Logger.LogInfo().Msg("GetAll successfully")
 	return c.JSON(http.StatusOK, domain.Response{
 		Meta: domain.Meta{
 			Count: len(products),
@@ -26,8 +30,11 @@ func GetById(c echo.Context) error {
 	id := c.Param("id")
 	product, err := ProductModel.GetById(c.Request().Context(), id)
 	if err != nil {
+		log.Error().Msg("Error in GetById of product: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
+
+	log.Info().Msg("GetById successfully")
 	return c.JSON(http.StatusOK, domain.Response{
 		Meta: domain.Meta{
 			Count: 1,
@@ -40,14 +47,17 @@ func Create(c echo.Context) error {
 	payload := new(domain.ProductInputDTO)
 	err := c.Bind(payload)
 	if err != nil {
+		log.Error().Msg("bind error in struct: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	product, err := ProductModel.Create(c, payload)
 	if err != nil {
+		log.Error().Msg("Error in Create: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
+	log.Info().Msg("Create with successfully")
 	return c.JSON(http.StatusOK, domain.Response{
 		Meta: domain.Meta{
 			Count: 1,
@@ -61,13 +71,17 @@ func Update(c echo.Context) error {
 	payload := new(domain.ProductInputDTO)
 	err := c.Bind(payload)
 	if err != nil {
+		log.Error().Msg("bind error in update: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
 
 	err = ProductModel.Update(c.Request().Context(), payload, id)
 	if err != nil {
+		log.Error().Msg("Error in Update: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
+
+	log.Info().Msg("Create with successfully")
 	return c.JSON(http.StatusOK, domain.Response{
 		Meta: domain.Meta{
 			Count: 1,
@@ -80,8 +94,11 @@ func Delete(c echo.Context) error {
 	id := c.Param("id")
 	err := ProductModel.Delete(c.Request().Context(), id)
 	if err != nil {
+		log.Error().Msg("Error in Delete: " + err.Error())
 		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 	}
+
+	log.Info().Msg("Deleted with successfully")
 	return c.JSON(http.StatusOK, domain.Response{
 		Meta: domain.Meta{
 			Count: 1,
--- main.go
+++ main.go
@@ -1,24 +1,27 @@
 package main
 
 import (
+	"eulabs/common"
 	"eulabs/domain"
 	ProductRouter "eulabs/internal/product/router"
-	"fmt"
-	"log"
 	"net/http"
+
+	"github.com/rs/zerolog/log"
 
 	"github.com/labstack/echo"
 )
 
 func main() {
 
-	fmt.Println("running application..")
+	log.Info().Msg("Server started")
 
 	e := echo.New()
+	common.NewLogger()
+	e.Use(common.LoggingMiddleware)
 	domain.GlobalConfig.LoadVariables()
 
 	ProductRouter.RegisterRoutes(e)
 	if err := e.Start(":8080"); err != http.ErrServerClosed {
-		log.Fatal(err)
+		log.Error().Msg("Error message: " + err.Error())
 	}
 }
